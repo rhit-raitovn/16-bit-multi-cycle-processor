9e8082f43e96a7c4fb3078fa739bc63ffc3010bf
LukePearcy <115423733+LukePearcy@users.noreply.github.com>
2024-02-13 19:53:48 -0500

    memory component and test-bench working
    we still need to ensure it meets his conditions and supports input/output at certain adresses

--- implementation/memory_component.v
+++ implementation/memory_component.v
@@ -1,7 +1,7 @@
 // Quartus Prime Verilog Template
 // Single port RAM with single read/write address 
 
-module memory 
+module memory_component 
 (
 	input [15:0] data,
 	input [15:0] addr,
@@ -11,15 +11,15 @@ module memory
 );
 parameter ADDR_WIDTH=10;
 	// Declare the RAM variable
-	reg [9:0] ram[0:2**ADDR_WIDTH-1];
+	reg [15:0] ram[0:2**ADDR_WIDTH-1];
 
 	// Variable to hold the registered read address
 	reg [15:0] addr_reg;
 
 	
 	initial begin
-		$display("atempting to find memory.mem");
-    		$readmemh("memory.mem", ram);
+		$display("atempting to find memory.txt");
+    		$readmemh("C:/Users/pearcyln/OneDrive - Rose-Hulman Institute of Technology/Documents/GitHub/rhit-csse232-2324b-project-lime-2324b-02/implementation/memory.txt", ram);//opens file here
 	end
 	
 	always @ (posedge clk)
@@ -28,7 +28,7 @@ parameter ADDR_WIDTH=10;
 		if (we)
 			ram[addr] <= data;
 
-		   addr_reg <= addr;
+		addr_reg <= addr;
 	end
 
 	// Continuous assignment implies read returns NEW data.
--- implementation/tb_memory.v
+++ implementation/tb_memory.v
@@ -1,14 +1,56 @@
 module tb_memory;
 
+//inputs
+reg [15:0] input_data;
+reg [15:0] input_addr;
+reg input_write;
+reg CLK;
+
+//outputs
 wire [15:0] output_q;
 
-MEMORY uut(
-	.data(16'b0),
-	.addr(10'b0),
-	.we(1'b0), 
-	.clk(1'b0),
+memory_component uut(
+	.data(input_data),
+	.addr(input_addr),
+	.we(input_write), 
+	.clk(CLK),
 	
 	.q(output_q)
 );
+
+
+// Clock generation
+    initial CLK=0;
+  always begin
+    #5 CLK = ~CLK; // Toggle the clock every 5 time units
+  end
+
+initial begin
+	//TEST 0 mem is properly loaded
+	input_addr=16'h0000;
+	input_write=1'b0;
+	#10;
+	if(output_q!==16'hdead)
+		$display("FAIL test 0 in memory_component: adress 0 was expected to be hexadecimal value 'dead' but was: %h (if this is all X's the memory did not load, make sure filepath is correct)",output_q);
+	
+	//TEST 1 write to mem
+	input_addr=16'h0001;
+	input_write=1'b1;
+	input_data=16'h0420;
+	#10;
+	if(output_q!==16'h0420)
+		$display("FAIL test 1 in memory_component: atempt to write hexadecimal value 420 to address 1 failed, output was:%h",output_q);
+	
+	//TEST 2 read from address previously written to
+	input_addr=16'h0000;
+	#10;
+	input_addr=16'h0001;
+	#10;
+	if(output_q!==16'h0420)
+		$display("FAIL test 2 in memory_component: adress 1 did not keep hex value 420, output was:%h",output_q);
+
+	$stop;
+
+end
 
 endmodule
