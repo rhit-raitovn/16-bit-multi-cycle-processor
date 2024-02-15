403291924c59258604c0f41741f81fe278187e5c
rhit-raitovn <116213728+rhit-raitovn@users.noreply.github.com>
2024-02-14 21:53:22 -0500

    Update TheLime.v

--- implementation/TheLime.v
+++ implementation/TheLime.v
@@ -19,12 +19,10 @@ wire [1:0] branchType;
 
 wire CLK;
 
-wire [15:0] input_newPC;
 wire CLK;
 wire input_zero;
 wire input_negative;
     
-// External outputs
 wire [15:0] output_PC,
 wire [6:0] Output_IR_Control,
 wire [2:0] Output_IR_RegA,
@@ -34,18 +32,21 @@ wire [15:0] Output_IR_Imm,
 wire [15:0] input_from_ALUOut,
 wire [15:0] output_MDR,
 wire [15:0] input_mem_data;
+
+// wire output_Zero;
+// wire output_negative;
   
 FetchAndMemory fetch_and_memory_inst (
   // External inputs and output
   .input_PC_PCWrite(PCWrite),
-  .input_PC_newPC(input_newPC),
+  .input_PC_newPC(output_ALUMuxOut),
   .CLK(CLK),
-  .input_zero(input_zero),
-  .input_negative(input_negative),
+  .input_zero(output_Zero),
+  .input_negative(output_negative),
   .input_branchType(branchType),
   .input_PC_isbranch(branch),
   .input_IR_write(input_IR_write),
-  .input_from_ALUOut(input_from_ALUOut),
+  .input_from_ALUOut(output_ALUOut),
   .IorD(IorD),
   .input_mem_write(memW),
   .input_mem_data(input_mem_data)
@@ -72,7 +73,7 @@ Data data_inst (
   .input_reg_write_address(Output_IR_RegD),
   .CLK(CLK),
   .input_imm(Output_IR_Imm),
-  .input_ALUOut(ALUOut),
+  .input_ALUOut(output_ALUOut),
   .input_MDR(output_MDR),
   .memToReg(mem2reg),
         
@@ -82,6 +83,9 @@ Data data_inst (
   .output_reg_B(output_reg_B)
 );
 
+wire [15:0] output_ALUOut;
+wire [15:0] output_ALUMuxOut;
+  
 Calculations calculations_inst (
   // Inputs
   .input_A(output_reg_A),
@@ -95,7 +99,8 @@ Calculations calculations_inst (
   .clk(CLK),
   
   // Outputs
-  .output_ALU(output_ALU),
+  .output_ALU(output_ALUOut),
+  .output_ALU(output_ALUMuxOut),
   .output_Zero(output_Zero),
   .output_negative(output_negative)
 );
