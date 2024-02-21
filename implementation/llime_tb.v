babd711577734c2607d603ed257db69643cef8a1
LukePearcy <115423733+LukePearcy@users.noreply.github.com>
2024-02-20 20:01:36 -0500

    Update llime_tb.v

--- implementation/llime_tb.v
+++ implementation/llime_tb.v
@@ -4,23 +4,37 @@ module lime_tb();
 reg [15:0] main_input;
 wire [15:0] main_output;
 wire internalClock;
+reg CLK;
 
 TheLime uut(
 	.main_input(main_input),
 	.main_output(main_output),
-	.CLK(internalClock)
+	.CLK(CLK)
 );
 
+integer cycleCount=0;
+
+
+
+always begin
+  #5 
+  CLK=~CLK;
+  cycleCount=cycleCount+1;
+ end
+
+
 initial begin
+	CLK=1;
 	//test 1 relprime
-	main_input=16'h0006;
+	main_input=16'h13b0;
 	//#500;
 	//if(main_output!==16'h0005)
 	//	$display("lime processor test failed :( output was: 0x%h",main_output);
 	//else
 	//	$display("TEST PASSED! :)");
 	//$stop;
-	@(main_output==16'h0005);
+	@(main_output==16'h000b);
+	$display(cycleCount/2);
 #10
 	$stop;
 end
